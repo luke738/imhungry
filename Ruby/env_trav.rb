45b62703c8a504ee844db9b33cdc9eb89dee48f9
Luke St. Regis <stregis@usc.edu>
2019-04-24 12:24:31 -0700

    Tweak travis so that it uses a different cucmber env.rb file than we do

---
+++ Ruby/env_trav.rb
@@ -0,0 +1,18 @@
+
+require 'capybara'
+require 'capybara/cucumber'
+require 'rspec'
+require 'webdrivers'
+
+
+#Capybara.register_driver :chrome do |app|
+  #Capybara::Selenium::Driver.new(app, :browser => :chrome, :driver_path=>"/Users/mackenziemcclung/Downloads/chromedriver")
+#end
+
+#Capybara.default_driver = :chrome
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.default_driver = :selenium
